aba76756dc0d34406c209c9e9dcf42c2f31b3212
aditya <aditya003.ay@gmail.com>
2021-09-17 21:15:20 +0530

    Increment IntentService

--- app/src/main/java/com/example/android/background/MainActivity.java
+++ app/src/main/java/com/example/android/background/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.android.background;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
@@ -10,6 +11,8 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.android.background.sync.ReminderTasks;
+import com.example.android.background.sync.WaterReminderIntentService;
 import com.example.android.background.utilities.PreferenceUtilities;
 
 public class MainActivity extends AppCompatActivity implements
@@ -65,11 +68,17 @@ public class MainActivity extends AppCompatActivity implements
     public void incrementWater(View view) {
         if (mToast != null) mToast.cancel();
         mToast = Toast.makeText(this, R.string.water_chug_toast, Toast.LENGTH_SHORT);
-        mToast.show();
+
         // TODO (15) Create an explicit intent for WaterReminderIntentService
+        Intent incrementWaterCountIntent = new Intent(this , WaterReminderIntentService.class);
+
         // TODO (16) Set the action of the intent to ACTION_INCREMENT_WATER_COUNT
+        incrementWaterCountIntent.setAction(ReminderTasks.ACTION_INCREMENT_WATER_COUNT);
+
         // TODO (17) Call startService and pass the explicit intent you just created
+        startService(incrementWaterCountIntent);
     }
+
 
     @Override
     protected void onDestroy() {
